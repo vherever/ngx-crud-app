dd5c3a2a28a133b52e22d2cf43cd263432e3cce5
vherever <vherever@gmail.com>
2018-05-06 13:49:37 +0300

    Add collapse action on post title click

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Post} from '../../models/post';
 
 @Component({
   selector: 'app-posts',
@@ -6,9 +7,13 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
-  @Input() posts: any;
+  @Input() posts: Post[];
 
   constructor() { }
+
+  public onPostClick(post: Post): void {
+    post._collapsed = !post._collapsed;
+  }
 
   ngOnInit() {
   }
