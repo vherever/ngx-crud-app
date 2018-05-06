b350fc34935936f14266aa27aa167106aa5550ce
vherever <vherever@gmail.com>
2018-05-06 12:12:24 +0300

    app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { CommentsComponent } from './components/comments/comments.component';
+import { CommentComponent } from './components/comments/comment/comment.component';
+import { PostComponent } from './components/posts/post/post.component';
+import { AddPostComponent } from './components/posts/add-post/add-post.component';
+import { AddCommentComponent } from './components/comments/add-comment/add-comment.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    CommentsComponent,
+    CommentComponent,
+    PostComponent,
+    AddPostComponent,
+    AddCommentComponent
   ],
   imports: [
     BrowserModule
